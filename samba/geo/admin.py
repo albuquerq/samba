9fc150b432ad20c8ab656219f2b040591c3bcd97
Natan Albuquerque <natanalbuquerque5@gmail.com>
2016-08-23 02:45:16 -0300

    Criado app "geo"

---
+++ samba/geo/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from . import models
+
+# Register your models here.
+admin.site.register(models.Municipio)
+admin.site.register(models.UF)
---
+++ samba/geo/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+
+class UF(models.Model):
+	"""
+	UF: Unidade da federação, corresponde a um estado do país.
+	"""
+	nome = models.CharField(max_length=100)
+	sigla = models.CharField(max_length=4)
+	regiao = models.CharField(max_length=50)
+
+	def __str__(self):
+		return self.nome
+
+class Municipio(models.Model):
+	"""
+	Representa uma cidade.
+	"""
+	nome = models.CharField(max_length=200)
+	cod_ibge = models.IntegerField()
+	descricao = models.TextField()
+	UF = models.ForeignKey("UF")
+	
+	#avatar = models.ImageField(upload_to="municipios")
+
+	lat = models.FloatField()
+	lng = models.FloatField()
+
+	def __str__(self):
+		return self.nome
